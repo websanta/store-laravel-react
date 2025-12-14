e36f1a1bbad8312ca2f4b9d42d0b609e6907ace7
websanta <websanta@websanta.ru>
2025-12-14 03:14:36 +0300

    Fix: individual Delete option added to Products

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -120,6 +120,7 @@ class ProductResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([

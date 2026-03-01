ef8022ef68e93f444fe641d25872108b7c68f3f7
websanta <websanta@websanta.ru>
2026-03-01 19:15:41 +0300

    Single Product Page Choosing Picture FIXED

--- resources/js/Components/Core/Carousel.tsx
+++ resources/js/Components/Core/Carousel.tsx
@@ -1,24 +1,39 @@
 import {Image} from '@/types';
+import {useEffect, useState} from 'react';
 
 function Carousel({images}: {
   images: Image[]
 }) {
+  const [selectedImage, setSelectedImage] = useState<Image>(images[0]);
+
+  useEffect(() => {
+    setSelectedImage(images[0]);
+  }, [images]);
+
   return (
     <>
       <div className="flex items-start gap-8">
         <div className="flex flex-col items-center gap-2 py-2">
           {images.map((image, i) => (
-            <a href={'#item' + i} className="border-2 hover:border-blue-500" key={image.id}>
+            <button
+            onClick={ev => setSelectedImage(image)}
+            className={`
+                border-2
+                ${selectedImage.id === image.id
+                    ? 'border-4 border-blue-700'
+                    : 'border-2 hover:border-blue-500'
+                }
+            `}
+            key={image.id}>
               <img src={image.thumb} alt="" className="w-[50px]" />
-            </a>
+            </button>
           ))}
         </div>
         <div className="carousel w-full">
-          {images.map((image, i) => (
-            <div id={'item' + i} className="carousel-item w-full" key={image.id}>
-              <img src={image.large} className="w-full" alt="" />
-            </div>
-          ))}
+          <div className="carousel-item w-full">
+            <img
+            src={selectedImage.large} className="w-full" alt="" />
+          </div>
         </div>
       </div>
     </>

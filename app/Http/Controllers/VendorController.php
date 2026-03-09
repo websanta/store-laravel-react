f58fcfa4ece14f8b88454b1fff0a627929813a5e
websanta <websanta@websanta.ru>
2026-03-09 19:36:21 +0300

    Commit before laravel-stripe-connect pkg installation

---
+++ app/Http/Controllers/VendorController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vendor;
+use Illuminate\Http\Request;
+
+class VendorController extends Controller
+{
+    public function prifile(Vendor $vendor)
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+}
